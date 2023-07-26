01e90248fda0f37568060221099c560a1c822cdc
tranquochuy645 <119860259+tranquochuy645@users.noreply.github.com>
2023-07-26 13:15:17 +0000

    Commit by Github Action

--- src/middlewares/express/multerUpload/index.js
+++ src/middlewares/express/multerUpload/index.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.multerUpload = void 0;
 const multer_1 = __importDefault(require("multer"));
+const fs_1 = __importDefault(require("fs"));
 const storage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
-        // You can determine the destination directory based on an argument or any other logic here
-        const dynamicPath = `media/${req.params.userId}/${req.params.roomId}`; // Replace this with your logic
+        const dynamicPath = `media/${req.params.userId}/${req.params.roomId}`;
+        // Create the directory if it doesn't exist
+        fs_1.default.mkdirSync(dynamicPath, { recursive: true });
         cb(null, dynamicPath);
     },
     filename: (req, file, cb) => {
@@ -16,5 +18,5 @@ const storage = multer_1.default.diskStorage({
         cb(null, uniqueSuffix + file.originalname);
     },
 });
-const multerUpload = (0, multer_1.default)({ storage: storage }).single('file'); // Specify the field name for the uploaded file
+const multerUpload = (0, multer_1.default)({ storage: storage }).single('file');
 exports.multerUpload = multerUpload;
